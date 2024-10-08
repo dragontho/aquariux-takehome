7b83812d83cb4a2cd35ca950721d9d87cf9e0c3c
therealdragontho <151636536+therealdragontho@users.noreply.github.com>
2024-10-08 11:08:08 +0800

    Initial Springboot boilerplate

---
+++ src/main/java/com/dragontho/aqtakehome/AqtakehomeApplication.java
@@ -0,0 +1,13 @@
+package com.dragontho.aqtakehome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AqtakehomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AqtakehomeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dragontho/aqtakehome/AqtakehomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dragontho.aqtakehome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AqtakehomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
